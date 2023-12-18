227a1e45a2d6f13b9cbc80c14bfa2f81a249e995
Derlis Damian Meza Carballo <derlisdamianmeza@gmail.com>
2023-12-18 10:11:28 -0300

    initial commit

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import { generate } from "random-words";
+import { Server, Socket } from "socket.io";
+
+dotenv.config();
+
+const io = new Server({
+  cors: {
+    origin: process.env.CLIENT_URL,
+  }
+});
+
+io.on("connection", (socket: Socket) => {
+  socket.on("finish", () => {
+    socket.emit("new word", generate());
+  });
+
+  socket.emit("initialize", generate(), generate())
+});
+
+io.listen(Number(process.env.PORT) || 3000);
